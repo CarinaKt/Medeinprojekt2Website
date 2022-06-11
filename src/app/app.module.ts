d11f9f62fe2bcb7a466b306277f14775cca42e2c
CarinaKt <63025657+CarinaKt@users.noreply.github.com>
2022-06-11 10:46:15 +0200

    Fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FirstPageComponent } from './pages/first-page/first-page.component';
 import { SecondPageComponent } from './pages/second-page/second-page.component';
 import { MatIconModule} from "@angular/material/icon";
-import {ToolbarWithNavModule} from "./toolbarWithNav/toolbarWithNav.module";
 import {CommonModule} from "@angular/common";
 import { MatToolbarModule} from "@angular/material/toolbar";
 import {MatSidenavModule} from "@angular/material/sidenav";
@@ -21,7 +18,6 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     ToolbarComponent,
     FirstPageComponent,
     SecondPageComponent,
@@ -31,7 +27,6 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ToolbarWithNavModule,
     CommonModule,
     MatToolbarModule,
     MatIconModule,
